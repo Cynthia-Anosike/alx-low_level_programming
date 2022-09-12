8ba81753e6dc7064692f2a5674c1f99867c0f326
Cynthia Chioma Anosike <chiomaanosike28@gmail.com>
2022-09-12 11:46:36 +0000

    .Task4.

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - This code prints all letters except e and q
+ * followed by a white line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c != 'e' && c != 'q')
+		{
+			putchar(c);
+		}
+		c++;
+		putchar('\n');
+		return (0);
+	}
+}
