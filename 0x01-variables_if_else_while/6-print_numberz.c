5b05104c55065f8ae9cbefc60015658a83eced48
Cynthia Chioma Anosike <chiomaanosike28@gmail.com>
2022-09-12 17:25:56 +0000

    T

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Print numbers of base 10 without
+ * printf, puts, followed by
+ * a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 10; n < 10; n++)
+	{
+		putchar(n);
+	}
+	
+	putchar('\n');
+	return (0);
+}	
