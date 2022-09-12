0b068a366ee0d184ce535d490e45724092f12d0b
Cynthia Chioma Anosike <chiomaanosike28@gmail.com>
2022-09-12 12:35:27 +0000

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - This code will print 10 base numbers
+ * starting with 0, followed by
+ * a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a = 0;
+	while (a <= 10)
+	{
+		printf("Value of a: %d\n", a);
+		a++;
+	}
+	return (0);
+}
