74f136fd7a1b706e3efde757a1608687753190c5
Cynthia Chioma Anosike <chiomaanosike28@gmail.com>
2022-09-12 10:53:41 +0000

    Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - To print an upper and lower case alphabets
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	for (c ='A'; c <= 'Z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
