9decad947d7aa5b4d128379e1377844b1e53fe47
Cynthia Chioma Anosike <chiomaanosike28@gmail.com>
2022-09-13 22:05:57 +0000

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - Entry print
+ * @n: the int to check
+ * Return: The absolute value of int
+ */
+int _abs(int)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
