c434c1df6803ce1d2ad110fdfe8af71529c0d273
Cynthia Chioma Anosike <chiomaanosike28@gmail.com>
2022-09-22 09:45:48 +0000

    Task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,11 +12,13 @@ char *_strcat(char *dest, char *src)
 	int j;
 
 	i = 0;
+
 	while (dest[1] != '\0')
 	{
 		i++;
 	}
 	j = 0;
+
 	while (src[j] != '\0')
 	{
 		dest[i] = src[j];
@@ -24,5 +26,6 @@ char *_strcat(char *dest, char *src)
 		j++;
 	}
 	dest[i] = '\0';
+
 	return (dest);
 }
