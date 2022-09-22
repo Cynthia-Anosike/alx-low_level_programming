f8e773ad8c1098d7fa651781ec80a5ac5f039604
Cynthia Chioma Anosike <chiomaanosike28@gmail.com>
2022-09-22 11:43:05 +0000

    Task 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -7,14 +7,13 @@
  */
 void reverse_array(int *a, int n)
 {
-	int i, j, tmp;
-
-	j = n - 1;
-
-	for (i = 0; 1 < n / 2; i++)
+	int i;
+	int temp;
+	
+	for (i = 0; i < n--; i++)
 	{
-		tmp = a[i];
-		a[i] = a[j];
-		a[j--] = tmp;
+		temp = a[i];
+		a[i] = a[n];
+		a[n] = temp;
 	}
 }
