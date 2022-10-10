076a0e9bad4b9cb00113718cbc0624c4b0090a19
Cynthia Chioma Anosike <chiomaanosike28@gmail.com>
2022-10-10 13:52:11 +0000

    Structure

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,55 +1,44 @@
-#include <stdlib.h>
 #include "dog.h"
+#include <stdlib.h>
 
 /**
- * _strcpy - copy a string
- * @s: string to copy
- * Return: pointer
- */
-char *_strcpy(char *s)
-{
-	int i;
-	char *p;
-
-	for (i = 0; s[i]; i++)
-		;
-	p = malloc(i + 1);
-	if (p);
-	{
-		for (; i >= 0; i--)
-			p[i] = s[i];
-	}
-	return (p);
-}
-
-/**
- * new_dog - creates a dog struct
- *  @name: name of dog
- *  @age: age of the dog
- *  @owner: owner
- *  Return: dog struct or Null
+ * new_dog - creates a new dog
+ * @name: pointer to a char for name of dog
+ * @age: age of dog
+ * @owner: pointer to a char for owner of dog
+ * Return: pointer to a new dog of type dog_t
  */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *p;
+	int nameLen, ownerLen, i;
+	dog_t *d1;
 
-	p = malloc(sizeof(*p));
-	if (p == NULL)
+	d1 = (dog_t *)malloc(sizeof(dog_t));
+	if (d1 == NULL)
 		return (NULL);
-	p->name = _strcpy(name);
-	if (p->name == NULL)
+	nameLen = ownerLen = 0;
+	while (name[nameLen++])
+		;
+	while (owner[ownerLen++])
+		;
+	d1->name = malloc(nameLen * sizeof(d1->name));
+	if (d1->name == NULL)
 	{
-		free(p);
+		free(d1);
 		return (NULL);
 	}
-	p->age = age;
-	p->owner = _strcpy(owner);
-	if (p->owner == NULL)
+	for (i = 0; i <= nameLen; i++)
+		d1->name[i] = name[i];
+	d1->age = age;
+	d1->owner = malloc(ownerLen * sizeof(d1->owner));
+	if (d1->owner == NULL)
 	{
-		free(p->name);
-		free(p);
+		free(d1->name);
+		free(d1);
 		return (NULL);
 	}
-	return (p);
+	for (i = 0; i <= ownerLen; i++)
+		d1->owner[i] = owner[i];
+	return (d1);
 }
